baa8b50a307aa9cc6c4570174f47e08cfbd9b15a
rushab14 <79597429+rushab14@users.noreply.github.com>
2022-04-25 11:39:58 +0530

    Add files via upload

---
+++ badge.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Badge extends member{
+
+    String name;
+    String description;
+}
+
+public void init_name(String name){
+    this.name=name;
+
+}
+public void init_description(String description){
+    this.description=description;
+
+}
